554f33a1bda452c469ebc77cf6a13ea4436f20f4
dortizrueda <71823198+dortizrueda@users.noreply.github.com>
2024-03-04 18:27:29 +0100

    carga inicial

---
+++ app/src/main/java/com/example/turismotfg/AdminRegister.java
@@ -0,0 +1,16 @@
+package com.example.turismotfg;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class AdminRegister {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_register);
+
+        // Puedes agregar aquí el código específico para la actividad del administrador
+        // Por ejemplo, configurar vistas, manejar eventos, etc.
+    }
+}
---
+++ app/src/main/java/com/example/turismotfg/UserProfile.java
@@ -0,0 +1,16 @@
+package com.example.turismotfg;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+    }
+}
