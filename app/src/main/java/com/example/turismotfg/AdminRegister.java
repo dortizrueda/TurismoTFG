62d9830ed232383b86b2208cbc105412f0ab597f
dortizrueda <71823198+dortizrueda@users.noreply.github.com>
2024-03-06 14:18:39 +0100

    carga inicial.

--- app/src/main/java/com/example/turismotfg/AdminRegister.java
+++ app/src/main/java/com/example/turismotfg/AdminRegister.java
@@ -3,8 +3,9 @@ package com.example.turismotfg;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class AdminRegister {
+public class AdminRegister extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
